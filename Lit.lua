85600c199977307188cb4764790f8f76971b4447
Uncharron <uncharron@gmail.com>
2025-04-07 21:57:10 +0700

    Update Lit.lua

--- Lit.lua
+++ Lit.lua
@@ -1,5 +1,6 @@
 local Games = {
   [87039211657390] = "https://raw.githubusercontent.com/Uncharron/Solo/refs/heads/main/Solo_A.lua", -- Arise-Crossover
+  [128336380114944] = "https://raw.githubusercontent.com/Uncharron/Solo/refs/heads/main/Solo_A.lua", -- Arise-Crossover Dungeon
   [16732694052] = "https://raw.githubusercontent.com/Uncharron/Code_X_HUB_FH.lua/refs/heads/main/Code_X_HUB_FH_xox.lua", -- Fisch-ANIMAL 
 }
 return Games
