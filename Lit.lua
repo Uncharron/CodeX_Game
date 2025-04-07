70483f131787d4a51f20dbfd38cb66f2337be8c7
Uncharron <uncharron@gmail.com>
2025-04-07 21:50:00 +0700

    Create Lit.lua

---
+++ Lit.lua
@@ -0,0 +1,5 @@
+local Games = {
+  [87039211657390] = "https://raw.githubusercontent.com/Uncharron/Solo/refs/heads/main/Solo_A.lua", -- Arise-Crossover
+  [16732694052] = "https://raw.githubusercontent.com/Uncharron/Code_X_HUB_FH.lua/refs/heads/main/Code_X_HUB_FH_xox.lua", -- Fisch-ANIMAL 
+}
+return Games
